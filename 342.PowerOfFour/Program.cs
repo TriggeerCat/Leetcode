b0c3ebc6520a11a2027683fc30eec679bfc6403a
Denys Crewford <140117377+TriggeerCat@users.noreply.github.com>
2023-10-23 18:15:35 +0300

    Add files via upload

---
+++ 342.PowerOfFour/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _342.PowerOfFour
+{
+    internal class Program
+    {
+        public static bool IsPowerOfFour(int n)
+        {
+            if (n <= 0)
+                return false;
+            else
+            {
+                double log = Math.Log(n, 4);
+                return Convert.ToInt32(log) == log;
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsPowerOfFour(-2147483648));
+        }
+    }
+}
