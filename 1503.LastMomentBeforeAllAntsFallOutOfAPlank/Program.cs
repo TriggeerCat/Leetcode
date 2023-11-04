a7146a553e27d6d64eed847b5d6a27f13bc107d0
Denys Crewford <140117377+TriggeerCat@users.noreply.github.com>
2023-11-04 10:21:40 +0200

    Add files via upload

---
+++ 1503.LastMomentBeforeAllAntsFallOutOfAPlank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _1503.LastMomentBeforeAllAntsFallOutOfAPlank
+{
+    internal class Program
+    {
+        private int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+        
+        public int GetLastMoment(int n, int[] left, int[] right)
+        {
+            if (left.Length == 0)
+                left = new int[] {0};
+
+            if (right.Length == 0)
+                right = new int[] { n };
+            
+            return (Max(left.Max(), n - right.Min()));
+        }
+        
+        public static void Main(string[] args)
+        {
+            int n = 7;
+            int[] left = {0,1};
+            int[] right = {7};
+            Console.WriteLine(new Program().GetLastMoment(n, left, right));
+        }
+    }
+}
