1156d1ec10d9074c02246b80aee8c209c415ff3d
Denys Crewford <140117377+TriggeerCat@users.noreply.github.com>
2023-11-08 21:29:55 +0200

    Add files via upload

---
+++ 1921.EliminateMaximumNumberOfMonsters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _1921.EliminateMaximumNumberOfMonsters
+{
+    internal class Program
+    {
+        public int EliminateMaximum(int[] dist, int[] speed)
+        {
+            int length = dist.Length;
+            float[] time = new float[length];
+            for (int i = 0; i < length; i++)
+                time[i] = (float)dist[i] /  speed[i];
+            Array.Sort(time);
+            for (int turn = 0; turn < length; turn++)
+            {
+                if (time[turn] <= turn)
+                    return turn;
+            }
+            return length;
+        }
+        
+        public static void Main(string[] args)
+        {
+            int[] dist = { 1, 1, 2, 3 };
+            int[] speed = { 1, 1, 1, 1 };
+            Console.WriteLine(new Program().EliminateMaximum(dist, speed));
+        }
+    }
+}
---
+++ 2849.DetermineIfACellIsReachableAtAGivenTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2849.DetermineIfACellIsReachableAtAGivenTime
+{
+    internal class Program
+    {
+        public bool IsReachableAtTime(int sx, int sy, int fx, int fy, int t)
+        {
+            if (sx == fx && sy == fy)
+                return t != 1;
+            return (t >= Math.Max(Math.Abs(fx - sx), Math.Abs(fy - sy)));
+        }
+    
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Program().IsReachableAtTime(1, 4, 1, 2, 1));
+        }
+    }
+}
